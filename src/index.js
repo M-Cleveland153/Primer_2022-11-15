3ad3ea57f4fe5f3c9a9e1a8047d11ddb46d56c27
M-Cleveland153 <m999cleveland@gmail.com>
2022-11-07 23:29:16 -0600

    passing all tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 export function calculateTotal (items, tax) {
   // TODO
+  let taxableItemsTotal = 0
+  let nonTaxableItemsTotal = 0 
+  for(let item of items){
+    if(item['taxable'] === false){
+      nonTaxableItemsTotal += item['price']
+    } else {
+      taxableItemsTotal += item['price'] + (item['price']*Math.abs(tax))
+    }
+  }
+  return nonTaxableItemsTotal + taxableItemsTotal
 }
+
